febb0d1e64aa9a47232dcfa2cc4aec4fbba68ef3
Zishan21830 <103773696+Zishan21830@users.noreply.github.com>
2026-09-09 10:18:29 +0530

    Learn the promises in the JS

--- promise.js
+++ promise.js
@@ -1,5 +1,14 @@
+/**
+ * A promises is an object representing the eventual completion (or failure) of an async operation and its resulting value.
+ * Promise state:
+ * A promise is always in one of the 3  mutually exclusive states:
+ * 1. Pending- Initail State (Neither fulfilled nor resolved)
+ * 2. Fulfilled- Operation completed successfully and the promise now has a value.
+ * 3. Rejected- Operation failed and the promise now hasa reason(error)
+ */
+
 const fetchUserData = new Promise((resolve, reject) => {
-  let success = true;
+  let success = true;  //Here could be a database query, network response or API calling.
   setTimeout(() => {
     if (success) {
       resolve({ id: 101, username: "Zishan" });
